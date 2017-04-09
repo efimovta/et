a1cefb4514c4cbf0b126ec2040be2ba03ea07760
Efimov Timur <efimovta@gmail.com>
2017-04-10 01:43:00 +0300

    fix filling

--- src/powertest/java/efimovta/store/DeviceStore.java
+++ src/powertest/java/efimovta/store/DeviceStore.java
@@ -4,10 +4,16 @@ package efimovta.store;
  * Created by EFIMOVAT on 08.04.2017.
  */
 public class DeviceStore {
-    public static void main(String[] args){
+    static {
+        StorageFiller.fillStorage();
+    }
+
+    public static void main(String[] args) {
+
         IDeviceStore iDeviceStore = getDeviceStore();
     }
-    public static IDeviceStore getDeviceStore(){
+
+    public static IDeviceStore getDeviceStore() {
         return new DeviceStoreImpl();
     }
 }
